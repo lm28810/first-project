3cea883e900bd6cef20d8290a1f6c0f3bc572dbd
Jazmine <lm28810@gmail.com>
2022-09-21 21:47:36 -0400

    starting navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Stack } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { logo } from '../Utils/Constans'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+<Stack direction="row" spacing={2} sticky>
+ 
+</Stack>
+      
+    </div>
   )
 }
 
