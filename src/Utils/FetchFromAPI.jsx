32520cc95370ff01473724b614f7c7c6f2ab1f09
Jazmine <lm28810@gmail.com>
2022-09-22 20:44:16 -0400

    api fetched

---
+++ src/Utils/FetchFromAPI.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+
+const BaseURL = 'https://youtube-v31.p.rapidapi.com/search'
+const options = {
+  
+  url: BaseURL,
+  params: {
+    maxResults: '50'
+  },
+  headers: {
+    'X-RapidAPI-Key': Process.env.REACT_APP_API_KEY,
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+
+export const FetchFromAPI = async (url) => {
+    const { data } = await axios.get(`${BaseURL}/${URL}`, options);
+    return data;
+}
+
